633e98ce2b765ac244e3ca591552ffa75a63d18d
cabusto <justinkwarren@gmail.com>
2014-08-28 01:36:38 +0000

    ignored .pyc files, added in sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 #from flask.ext.stormpath import StormpathManager
-#from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 
 #config
-#app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 #stormpath_manager = StormpathManager(app)
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 from app import views
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app import app
+from app import app, db
 #, db
 #from models import RotoEvilStat
 
