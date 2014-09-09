9cc0a254f0b8d523d5318bd35dc3c8c29c85d443
cabusto <justinkwarren@gmail.com>
2014-09-09 09:53:07 +0000

    added some panel switching in app.js. Not implemented in index yet

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -92,4 +92,17 @@
 		});
 		
 	}]);
+	
+	app.controller('PanelController', function() {
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		}
+
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		}
+	});
+
 })();
