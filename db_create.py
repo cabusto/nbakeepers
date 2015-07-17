49b2d463159b74cbb3f379181efc2dba3a031e8c
cabusto <justinkwarren@gmail.com>
2015-07-17 18:53:17 +0000

    updated db_create script to run with new folder structure

--- db_create.py
+++ db_create.py
@@ -1,12 +1,13 @@
 import fileinput
-from app import db
-from models import RotoEvilStat
+
+from project import app, db
+from project.models import RotoEvilStat
 
 db.drop_all()
 db.create_all()
 
 # import from .csv file
-for line in fileinput.input('app/static/data/rotoevil_tool.csv'):
+for line in fileinput.input('project/rotoevil/static/data/rotoevil_tool.csv'):
 	items = line.split(',')
 	r = RotoEvilStat(*items)
 	db.session.add(r)
